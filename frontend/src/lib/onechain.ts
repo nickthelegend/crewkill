a9367edff624855bf9f908b3c0f3dd4853203d4f
nickthelegend <testingtesla7@gmail.com>
2026-03-25 03:53:11 +0530

    Redeployed Contracts

--- frontend/src/lib/onechain.ts
+++ frontend/src/lib/onechain.ts
@@ -4,13 +4,13 @@ export const ONECHAIN_RPC = 'https://rpc-testnet.onelabs.cc:443';
 
 export const suiClient = new SuiClient({ url: ONECHAIN_RPC });
 
-export const PACKAGE_ID = '0x4099ecc30a1952995c83b4f185b68093583611e7934946b8bb73657a54e1a640';
+export const PACKAGE_ID = '0x10961bfbec509dfc9fcf0bd8b60524c4df7726f53d46a694badaac24a4170d13';
 
 // Module specific IDs - usually these are shared objects created during init or manual setup
-export const GAME_MANAGER_ID = '0x3b27263b05400d61dcb41a56d19d7954cc1a10fe396e03f51bd0c52d0586e990';
-export const WAGER_VAULT_ID = '0x862b3118447979427ec22fd8000abc21d6e9f7e2e53d7ed472b8d63ac7926163';
-export const AGENT_REGISTRY_ID = '0xc14e3f34ac679169cfe11e833a07f870e9c1ae0c7d17fe3f1c5e5a4d687ace93';
-export const MARKET_REGISTRY_ID = '0x9e6d1b8cc8e900ce301b0a257327b9467be6ff92f779e593e95facde8228f510';
+export const GAME_MANAGER_ID = '0xa88b39d297cafa2b347bf4c5b71dd5b8b0751a1e8b4fc6fe0aa6be42519d9d6b';
+export const WAGER_VAULT_ID = '0x04215756fa43aab514f0d32ce1efc278af389614e56a4ab55a9d5562c8194863';
+export const AGENT_REGISTRY_ID = '0x4864f93d3c3adf25eb91caca86addc8b11fe72f927393884d46c364c34e8e0b1';
+export const MARKET_REGISTRY_ID = '0xddd4cfea1f4f1ca989f614d675151afa584e316db115fac7526f8652e123a4ca';
 // New AMM and Token IDs
 export const CREW_TOKEN_TYPE = `${PACKAGE_ID}::crew_token::CREW_TOKEN`;
 export const AMM_POOL_ID = '0x704a1b1d2adb5ea07797195a850fe3dfb4eeaf58f66b8bc0f894b5f819c3395f';
